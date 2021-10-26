b8916e978207089e07b11815836d200bebda2375
Cameron Sampson <csampson.js@gmail.com>
2021-10-26 04:04:03 -0500

    Audio streaming

--- src/index.js
+++ src/index.js
@@ -6,6 +6,27 @@ const MAPBOX_TOKEN = process.env.MAPBOX_TOKEN;
 
 const map = Leaflet.map('map').setView(NOLA_COORDS, 10);
 
+const OSD = {
+  canvas: document.querySelector('#osd'),
+  template:  document.querySelector('#osd-content'),
+  update (station) {
+    const clone = this.template.content.cloneNode(true);
+    clone.querySelector('h1').textContent = `${station.callsign} ${station.frequency}`;
+    clone.querySelector('p').textContent = `${station.market_city}, LA`;
+    this.canvas.replaceChildren(clone);
+  }
+};
+
+const Player = {
+  element: document.querySelector('audio'),
+  play (station) {
+    OSD.update(station);
+    this.element.pause();
+    this.element.src = station.stream_url;
+    this.element.play();
+  }
+};
+
 Leaflet.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -23,4 +44,5 @@ stations.forEach(station => {
   }).addTo(map);
 
   marker.bindTooltip(`${station.callsign} ${station.frequency}`);
+  marker.on('click', () => Player.play(station));
 });
